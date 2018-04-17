a82a012db377d64c783970c39ce31d7ac2517b2d
Nuno Gomes <2170163@my.ipleiria.pt>
2018-04-17 13:01:14 +0100

    added USStepsDef files and first test done

---
+++ src/test/java/US1StepsDef.java
@@ -0,0 +1,46 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class US1StepsDef {
+    private WebDriver driver;
+
+    @Given("^I access the landing page of COS$")
+    public void iAccessTheLandingPageOfCOS() throws Throwable {
+        driver.get("http://35.190.213.163/qs-project-team1/");
+        assertEquals("Contacts Orchestrator Solution", driver.getTitle());
+    }
+
+    @Then("^the title of the page should be \"([^\"]*)\"$")
+    public void theTitleOfThePageShouldBe(String title) throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.titleContains(title));
+    }
+
+    @And("^I can see the text \"([^\"]*)\"$")
+    public void iCanSeeTheText(String text) throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.tagName("body")),text));
+    }
+
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+}
---
+++ src/test/java/US7StepsDef.java
@@ -0,0 +1,27 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class US7StepsDef {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+}
