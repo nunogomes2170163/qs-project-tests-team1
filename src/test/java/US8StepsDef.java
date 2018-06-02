0ee03124ab2ecf1b7569c12f1479549c96009b04
Joao280 <2170161@my.ipleiria.pt>
2018-06-03 00:51:56 +0100

    US8 - Conflicts Page
    Hope pipe gets green again ;)

---
+++ src/test/java/US8StepsDef.java
@@ -0,0 +1,104 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.TestCase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+
+public class US8StepsDef {
+
+    private WebDriver driver;
+    private String userGuidTable;
+    private String userFullNameTable;
+
+    @Before
+    public void setUp() {
+        System.setProperty("phantomjs.binary.path",
+                "drivers/phantomjs");
+        driver = new PhantomJSDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Given("^the user is on the COS landing page$")
+    public void theUserIsOnTheCOSLandingPage() throws Throwable {
+        driver.get("http://35.190.213.163/qs-project-team1/");
+        assertEquals("Contacts Orchestrator Solution", driver.getTitle());
+    }
+
+    @When("^the user clicks on the \"([^\"]*)\" button located bottom of the page title$")
+    public void theUserClicksOnTheButtonLocatedBottomOfThePageTitle(String string) throws Throwable {
+        WebElement openButton = (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(@href, 'resolve_conflicts.php')]")));
+        TestCase.assertEquals(openButton.getText(), string);
+        userGuidTable = driver.findElement(By.xpath("//table[@id='contacts']/tbody/tr/td")).getText();
+        userFullNameTable = driver.findElement(By.xpath("//table[@id='contacts']/tbody/tr/td[2]")).getText();
+        openButton.click();
+    }
+
+
+    @Then("^the page sub title should be \"([^\"]*)\" - USSeven$")
+    public void thePageSubTitleShouldBeUSSeven(String arg0) throws Throwable {
+        TestCase.assertEquals(driver.findElement(By.xpath("//h2")).getText(),arg0);
+    }
+
+    @Given("^the user is on the COS - resolve conflicts page$")
+    public void theUserIsOnTheCOSResolveConflictsPage() throws Throwable {
+        driver.get("http://localhost:8080/resolve_conflicts.php");
+        assertEquals("Contacts Orchestrator Solution", driver.getTitle());
+        assertEquals(driver.findElement(By.xpath("//h2")).getText(),"CONTACT CONFLICTS");
+    }
+
+
+    @When("^the user clicks on the \"([^\"]*)\" button$")
+    public void theUserClicksOnTheButton(String arg0) throws Throwable {
+        driver.findElement(By.xpath("//a[contains(text(),'< back')]")).click();
+    }
+
+
+    @Then("^the \"([^\"]*)\" screen should be displayed - USseve$")
+    public void theScreenShouldBeDisplayedUSseve(String arg0) throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.xpath("//h2")),arg0));
+    }
+
+    @Then("^it should appear a column with the title \"([^\"]*)\"$")
+    public void itShouldAppearAColumnWithTheTitle(String arg0) throws Throwable {
+        String str = driver.findElement(By.id(arg0)).getText();
+        String[] splited = str.split(" ");
+        assertEquals(splited[0] + " " + splited[1] + " " +splited[2], arg0);
+    }
+
+    @And("^there should be a button to resolve conflict and a button to keep all contacts on each entry of the column \"([^\"]*)\"$")
+    public void thereShouldBeAButtonToResolveConflictAndAButtonToKeepAllContactsOnEachEntryOfTheColumn(String arg0) throws Throwable {
+        String str = driver.findElement(By.xpath("//div[@id='" + arg0 + "']/h1/span")).getText();
+        String[] splited = str.split(" ");
+        int rows = Integer.parseInt(splited[0].substring(1));
+        for (int i = 1; i < rows; i++) {
+            assertEquals(driver.findElement(By.xpath("//div[" + i + "]/div[2]/a")).getText(), "resolve");
+            assertEquals(driver.findElement(By.xpath("//div[" + i + "]/div[2]/a[2]")).getText(), "keep all");
+        }
+    }
+
+    @And("^the column \"([^\"]*)\" should have \"([^\"]*)\" conflicts to resolve$")
+    public void theColumnShouldHaveConflictsToResolve(String arg0, String arg1) throws Throwable {
+        String str = driver.findElement(By.xpath("//div[@id='" + arg0 + "']/h1/span")).getText();
+        String[] splited = str.split(" ");
+        assertEquals(splited[0].substring(1), arg1);
+    }
+
+
+}
---
+++ src/test/resources/US8.feature
@@ -0,0 +1,67 @@
+Feature: Access to Contact Orchestrator Solution's (COS) Resolve Conflicts Page
+  As a user
+  I want to access to a list of contact conflicts on a resolve conflicts page
+  So that I can see the contact conflicts by name, email and phone number
+  Scenario: Click on resolve conflicts
+    Given the user is on the COS landing page
+    When the user clicks on the "Resolve conflicts" button located bottom of the page title
+    Then the page sub title should be "CONTACT CONFLICTS" - USSeven
+
+
+  Scenario: Return to COS landing page from the resolve conflicts page
+    Given the user is on the COS - resolve conflicts page
+    When the user clicks on the "Back" button
+    Then the "CONTACTS LIST" screen should be displayed - USseve
+
+
+  Scenario Outline: Resolve conflicts page - initial state
+    Given the user is on the COS - resolve conflicts page
+    Then it should appear a column with the title "<columnTitle>"
+    And there should be a button to resolve conflict and a button to keep all contacts on each entry of the column "<columnTitle>"
+    Examples:
+      | columnTitle |
+      | Conflicts By Name |
+      | Conflicts By Email |
+      | Conflicts By Phone |
+
+  Scenario Outline: Resolve conflicts page - number of conflicts
+    Given the user is on the COS - resolve conflicts page
+    Then it should appear a column with the title "<columnTitle>"
+    And the column "<columnTitle>" should have "<number>" conflicts to resolve
+    Examples:
+      | columnTitle | number |
+      | Conflicts By Name        | 22     |
+      | Conflicts By Email       | 15     |
+      | Conflicts By Phone       | 14     |
+
+
+  Scenario: Click on keep all of a contact conflict on the resolve conflicts page
+    Given the user is on the COS - resolve conflicts page
+    When the user clicks on the "keep all" button of the first entry on the "Conflicts By Name"
+    Then the conflicts with those contacts should disappear from all columns
+    And those contacts should be saved as different contacts
+
+  Scenario: Can export contacts list to CSV file
+    Given the user is on the COS - resolve conflicts page
+    When there are no contact conflicts to resolve
+    Then the button "Export contacts" should be visible
+    And the page should present a "There no conflicts to resolve" message
+
+
+  Scenario Outline: Check if contacts conflicts are detected correctly
+    Given the user is on the COS - resolve conflicts page
+    When there are contact conflicts to resolve
+    Then the all the contact conflicts on the column with the title <columnTitle> should be detected by <field>
+    Examples:
+      | columnTitle | field |
+      | Conflicts By Name | Name |
+      | Conflicts By Email | Email |
+      | Conflicts By Phone | Phone |
+
+
+  Scenario: Export contacts list to CSV file
+    Given all conflicts are resolved
+    And the export button is enable
+    And the user click on the button export
+    Then the browser should download the CSV file
+    And the CSV file should contain all the resolved conflict contacts
